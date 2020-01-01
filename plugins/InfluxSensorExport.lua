7bc16f3f9da4fac619f9c0807d422d57f972eb72
Hans Schillstrom <hans.schillstrom@magseis.com>
2020-01-01 20:26:55 +0100

    Influx DB Sender script
    Example of how to use the influx plugin
    
    Signed-off-by: Hans Schillstrom <hans.schillstrom@magseis.com>

---
+++ plugins/InfluxSensorExport.lua
@@ -0,0 +1,55 @@
+﻿-- Script to filter out sensor values
+
+local debug = true			-- Print debug data to console
+local verbose = false			-- Print even more debug data
+
+function onInit()
+	print("Sending sensor values to influx-DB via Plugin %s", os.date("%Y-%m-%d %X"))
+	local ifx = require "influx.DB"
+	if ifx == nil then
+		print("Influx sender plugin is not installed")
+		return
+	end
+end
+
+
+function onSensorValueUpdated(device, valueType, value, scale)
+	if verbose == true then
+		print("debug %s", os.date("%Y-%m-%d %X", os.time()))
+		print("Device %s id:, %s, value %s type:%s scale:%s",  device:name(), device:id(), value, valueType, scale)
+		print("--")
+	end
+
+	local ifx = require "influx.DB"
+	if ifx == nil then
+		print("Influx sender plugin is not installed")
+		return
+	end
+	if device:name() == nil or device:name() == "" then
+		return
+	end
+	local sc_str  = string.format("%s", scale)
+	local meas = 'misc'
+
+	if device:id() > 80 and device:id() < 85 then
+		meas = 'heating'
+	elseif valueType == 1 then
+		meas = 'temperature'
+	elseif valueType == 2 then
+		meas = 'humidity'
+	elseif valueType == 4 or valueType == 8 then
+		meas = 'rain'
+	elseif valueType >= 16 then
+		meas = 'weather'
+	end
+
+    ifx:send{
+        measurment=meas,
+        location='indoor',
+        devname=device:name(),
+        devid=device:id(),
+        data=value,
+        datatype=valueType,
+        scale=sc_str
+    }
+end
